129e20a7616af25ccde4789cb651748b6fef618c
Libby Abrams <libbycabrams@gmail.com>
2019-07-08 15:44:57 -0700

    Update def filters description

--- libbyabikeshare.py
+++ libbyabikeshare.py
@@ -12,7 +12,7 @@ DAY_DATA = ['all', 'monday', 'tuesday', 'wednesday', 'friday', 'saturday', 'sund
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    The user has ability to filter by city, month and day, wahoo!
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
